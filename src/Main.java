9a432821e837b587e0153f5798b90cf30a784284
WINLordSnow <69006937+WINLordSnow@users.noreply.github.com>
2020-09-07 23:00:57 +0400

    дома 07.09.2020

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+class Interval {
+    long start;
+    long end;
+    int max;
+}
+
 class Vakancy {
     private long start;
     private long end;
@@ -20,12 +26,63 @@ class Vakancy {
     }
 
     boolean crossing (Vakancy v) {
-        return (((v.start<=end)|(end < v.end))&((v.end>=start)|(start>v.start)));
+        return (((v.start<=end)&(end < v.end))|((v.end>=start)&(start>v.start)));
     }
 
 }
 
 public class Main {
+    static boolean ifCrossing (Vakancy[] vakancies) {
+        boolean findCross = false;
+        int i = 0;
+        while ((!findCross)&(i!=(vakancies.length-1))) {
+            for (int j=i+1; j < vakancies.length; j++) {
+                if (vakancies[i].crossing(vakancies[j])) {
+                    findCross = true;
+                    break;
+                }
+            }
+            i++;
+        }
+        return findCross;
+    }
+
+    static Interval input (Vakancy[] vakancies) {
+        if (vakancies.length==1) {
+            Interval v = new Interval();
+            v.start = vakancies[0].getDateStart();
+            v.end = vakancies[0].getDateEnd();
+            v.max = 1;
+            return v;
+        }
+        if (ifCrossing(vakancies)) {
+            int j=0;
+            Vakancy[] temp = new Vakancy[vakancies.length];
+            for (int i=0; i< vakancies.length-1;i++){
+                if (vakancies[i].crossing(vakancies[i+1])) {
+                    temp[j++] = vakancies[i+1];
+                }
+            }
+            Vakancy[] temp1 = new Vakancy[j];
+            for (int k=0; k<j; k++)
+                temp1[k] = temp[k];
+
+            Interval v1 = new Interval();
+            v1 = input(temp1);
+            v1.max++;
+            if ((vakancies[0].getDateStart()<= v1.end)&(vakancies[0].getDateStart()>=v1.start)) {
+                v1.start = vakancies[0].getDateStart();
+            }
+            if ((vakancies[0].getDateEnd()<= v1.end)&(vakancies[0].getDateEnd()>=v1.start)) {
+                v1.end = vakancies[0].getDateEnd();
+            }
+            return v1;
+        }
+
+
+        return (Integer.toString(maxVakancy) + " " + Long.toString(sumPeriods));
+    }
+
     static Vakancy[] sort(Vakancy[] Vakancy) {
         Vakancy[] tempVakancy = new Vakancy[Vakancy.length];
         for (Vakancy i : tempVakancy)
@@ -107,5 +164,7 @@ public class Main {
         sumPeriods += endPeriod - startPeriod + 1;
         System.out.println(quantityPeriods + " " + sumPeriods);
         System.out.println(allVacancy[0].crossing(allVacancy[1]));
+        System.out.println(ifCrossing(allVacancy));
+        System.out.println(input(allVacancy));
     }
 }
